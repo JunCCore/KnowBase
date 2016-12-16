46cd791f36f42a626c3a207d2949b0958d3a6a9e
jun.chen <JunCcore@gmail.com>
2016-12-16 16:29:33 +0800

    Test Map

---
+++ src/com/Map/TestMap.java
@@ -0,0 +1,40 @@
+package com.Map;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class TestMap {
+
+	public static void main(String[] args) {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("a", "apple");
+		map.put("b", "banana");
+		map.put("c", "cocole");
+		map.put("d", "duang");
+		String str = "aa";
+		String str1 = "a";
+		if (str1.equals(str)) {
+			Iterator<String> it = map.keySet().iterator();
+			while (it.hasNext()) {
+				String mapKey = it.next();
+				if ("c".equals(mapKey)) {
+					continue;
+				}
+				String mapValue = map.get(mapKey);
+				System.out.println("map 的键：" + mapKey + "   map的值：" + mapValue);
+			}
+		} else {
+
+			System.out.println("========");
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					System.out.println("线程start...");
+				}
+			}).start();
+		}
+
+	}
+
+}
