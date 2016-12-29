4e3ae7a3859ee176069bab69ec7355a506f70523
jun.chen <jun.chen@kaixiangtech.com>
2016-12-29 12:36:42 +0800

    ss

---
+++ src/com/Properties/PropertiesUtil.java
@@ -0,0 +1,15 @@
+package com.Properties;
+
+import java.io.File;
+
+public class PropertiesUtil {
+	private final static String filePath = "WEB-INF" + File.separator + "conf" + File.separator;
+
+
+	private static String getPropertiesValue(String fileName, String Key, String defaultValue) {
+		String file = filePath + fileName;
+		
+		return null;
+	}
+
+}
