bceafd9c3e57def12b2278173d479dddcf639d34
JunCcore <jun.chchore@gmail.com>
2023-09-24 08:45:05 +0800

    update

--- src/com/JVM/Test.java
+++ src/com/JVM/Test.java
@@ -2,33 +2,33 @@ package com.JVM;
 
 
 public class Test {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		/**
-		 * 条件表达式
-		 */
-		Object o = true ? new Integer(1) : new Double(2.0);
-		// System.out.println(o);
+        /**
+         * 条件表达式
+         */
+        Object o = (double) Integer.valueOf(1);
+        // System.out.println(o);
+        for (int i = 0; i < 10; i++) {
+            System.out.println((Integer) i);
+            System.out.println();
+        }
 
-		for (int i = 0; i < 10; i++) {
-			System.out.println((Integer) i);
-			System.out.println();
-		}
+    }
 
-	}
-	
-	/**
-	 * 
-	 * Exception
-	 * 
-	 * @param e
-	 */
-	static void doThrow(Exception e) {
-		Test.<RuntimeException> doThrow0(e);
-	}
 
-	@SuppressWarnings("unchecked")
-	static <E extends Exception> void doThrow0(Exception e) throws E {
-		throw (E) e;
-	}
+    /**
+     * Exception
+     *
+     * @param e
+     */
+    static void doThrow(Exception e) {
+        Test.doThrow0(e);
+    }
+
+
+    @SuppressWarnings("unchecked")
+    static <E extends Exception> void doThrow0(Exception e) throws E {
+        throw (E) e;
+    }
 }
